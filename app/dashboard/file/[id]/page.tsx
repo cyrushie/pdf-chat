ea351935a5a6df5ea008bed05c7861ee2ac10c10
cyrus <cymine2404@gmail.com>
2025-10-05 18:53:51 +0800

    fixed more ts errorsss

--- app/dashboard/file/[id]/page.tsx
+++ app/dashboard/file/[id]/page.tsx
@@ -3,42 +3,36 @@ import PdfView from "@/components/pdfView";
 import { prisma } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 
-interface FilePageProps {
-  params: { id: string };
-}
+type ChatToFilePageProps = {
+  params: {
+    id: string;
+  };
+};
 
-const ChatToFilePage = async ({ params }: FilePageProps) => {
+const ChatToFilePage = async ({ params }: ChatToFilePageProps) => {
   auth.protect();
   const { userId } = await auth();
-  const { id } = params; // ✅ no await
+  const { id } = params; // ✅ just an object
 
   if (!userId) {
     throw new Error("User not found");
   }
 
   const data = await prisma.file.findUnique({
-    where: {
-      id,
-      userId,
-    },
+    where: { id, userId },
   });
 
   if (!data) {
     throw new Error("Missing data");
   }
 
-  const url = data.downloadUrl;
-
   return (
     <div className="grid lg:grid-cols-5 h-full overflow-hidden">
-      {/* Right */}
       <div className="col-span-5 lg:col-span-2 overflow-y-auto">
         <Chat id={id} />
       </div>
-
-      {/* Left */}
       <div className="col-span-5 lg:col-span-3 overflow-auto border-r-2 lg:border-indigo-600 bg-gray-100 lg:-order-1">
-        <PdfView url={url} />
+        <PdfView url={data.downloadUrl} />
       </div>
     </div>
   );
