abd5c90b9c5491328fcc77690a07f7f843a88f76
cyrus <cymine2404@gmail.com>
2025-10-05 18:36:34 +0800

    fixed more ts errors

--- app/dashboard/file/[id]/page.tsx
+++ app/dashboard/file/[id]/page.tsx
@@ -3,10 +3,14 @@ import PdfView from "@/components/pdfView";
 import { prisma } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 
-const ChatToFilePage = async ({ params }: { params: { id: string } }) => {
+interface FilePageProps {
+  params: { id: string };
+}
+
+const ChatToFilePage = async ({ params }: FilePageProps) => {
   auth.protect();
   const { userId } = await auth();
-  const { id } = params;
+  const { id } = params; // ✅ no await
 
   if (!userId) {
     throw new Error("User not found");
@@ -29,16 +33,15 @@ const ChatToFilePage = async ({ params }: { params: { id: string } }) => {
     <div className="grid lg:grid-cols-5 h-full overflow-hidden">
       {/* Right */}
       <div className="col-span-5 lg:col-span-2 overflow-y-auto">
-        {/* Chat */}
         <Chat id={id} />
       </div>
 
       {/* Left */}
       <div className="col-span-5 lg:col-span-3 overflow-auto border-r-2 lg:border-indigo-600 bg-gray-100 lg:-order-1">
-        {/* PDF View */}
         <PdfView url={url} />
       </div>
     </div>
   );
 };
+
 export default ChatToFilePage;
